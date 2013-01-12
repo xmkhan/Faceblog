9d7e2b6b37c1491d5e166fe9fa431ab0f18b60ac
Mohammad Khan <mohkhan@zynga.com>
2013-01-11 21:37:44 -0500

    Add auth login, more problems

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.configure('development', function () {
 });
 
 app.get('/', routes.index);
+app.get('/auth', routes.index);
+app.get('/trending', routes.index);
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
---
+++ routes/auth.js
@@ -0,0 +1,52 @@
+/**
+ * GET trending page
+ */
+
+var graph = require('fbgraph');
+
+// Module level constants
+var CLIENT_ID = '394935803933737';
+var APP_SECRET = 'e209282a5bd5b1836d838e24b17652ee';
+var REDIRECT_URI = 'http://faceblog.herokuapp.com/auth';
+
+var options = {
+    timeout:  3000,
+    pool:     { maxSockets:  Infinity },
+    headers:  { connection:  "keep-alive" }
+  };
+
+exports.index = function (req, res) {
+  // get authorization url
+  if (!req.query.code) {
+    var authUrl = graph.getOauthUrl({
+      "client_id"   :     CLIENT_ID,
+      "redirect_uri":  REDIRECT_URI
+    });
+    if (!req.query.error) {
+      res.redirect(authUrl);
+    } else {
+      console.log(req.query.error);
+      res.send('access denied');
+    }
+    return;
+  }
+
+  // after user click, auth `code` will be set
+  // we'll send that and get the access token
+  graph.authorize({
+    "client_id":      CLIENT_ID,
+    "redirect_uri":   REDIRECT_URI,
+    "client_secret":  APP_SECRET,
+    "code":           req.query.code
+  }, function (err, facebookRes) {
+    console.log(facebookRes);
+    graph
+      .setOptions(options)
+      .get("me", function (err, res) {
+        console.log(res);
+      });
+
+    res.redirect('/trending');
+  });
+
+};
