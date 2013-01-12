b4191cbfd8a90cc80a4888e53ec8239cd687206c
Mohammad Khan <mohkhan@zynga.com>
2013-01-11 20:10:15 -0500

    Heroku update: test 1

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,32 @@
 /*
  * GET home page.
  */
+var graph = require('fbgraph');
+
+// Module level constants
+var CLIENT_ID = '394935803933737';
+var APP_SECRET = 'e209282a5bd5b1836d838e24b17652ee';
+var REDIRECT_URI = 'http://faceblog.herokuapp.com';
 
 exports.index = function (req, res) {
-  res.render('index', { title: 'Express' });
+  // get authorization url
+  var authUrl = graph.getOauthUrl({
+    "client_id"   :     CLIENT_ID,
+    "redirect_uri":  REDIRECT_URI
+  });
+
+
+  // after user click, auth `code` will be set
+  // we'll send that and get the access token
+  graph.authorize({
+    "client_id":      CLIENT_ID,
+    "redirect_uri":   REDIRECT_URI,
+    "client_secret":  APP_SECRET,
+    "code":           req.query.code
+  }, function (err, facebookRes) {
+    res.redirect('/loggedIn');
+  });
+
+  // shows dialog
+  res.redirect(authUrl);
 };
