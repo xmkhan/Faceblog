b9b86e2e0407fa04fd3e9a2838a8214101aca13f
Mohammad Khan <mohkhan@zynga.com>
2013-01-11 21:01:32 -0500

    Heroku test 5

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,20 @@ var REDIRECT_URI = 'http://faceblog.herokuapp.com';
 
 exports.index = function (req, res) {
   // get authorization url
-  var authUrl = graph.getOauthUrl({
-    "client_id"   :     CLIENT_ID,
-    "redirect_uri":  REDIRECT_URI
-  });
+  if (!req.query.code) {
+    var authUrl = graph.getOauthUrl({
+      "client_id"   :     CLIENT_ID,
+      "redirect_uri":  REDIRECT_URI
+    });
+    if (!req.query.error) {
+      res.redirect(authUrl);
+    } else {
+      console.log(req.query.error);
+      res.send('access denied');
+    }
+    return;
+  }
 
-  /**
   // after user click, auth `code` will be set
   // we'll send that and get the access token
   graph.authorize({
@@ -28,8 +36,5 @@ exports.index = function (req, res) {
     console.log(facebookRes);
     if (facebookRes && !facebookRes.error) res.redirect('/loggedIn');
   });
-  */
 
-  // shows dialog
-  res.redirect(authUrl);
 };
